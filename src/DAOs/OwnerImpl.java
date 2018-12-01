5330a5177ca2fe212b8c37e58a84948ca81cb822
Nick <nikos_sourvi@hotmail.com>
2018-12-01 17:44:34 +0200

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	src/Utilities/Menu.java

--- src/DAOs/OwnerImpl.java
+++ src/DAOs/OwnerImpl.java
@@ -4,7 +4,6 @@ import DBUtils.DatabaseConnection;
 import Entities.Owner;
 import Entities.Vehicle;
 
-import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
